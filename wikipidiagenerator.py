a2e7b6ce860227024c1126dcd1160694c4dca1ee
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-04-06 22:53:56 +0300

    Made changes

--- wikipidiagenerator.py
+++ wikipidiagenerator.py
@@ -1,3 +1,25 @@
+
 import requests
+from bs4 import BeautifulSoup
 import webbrowser
 
+
+while True:
+    #used to produce articles from Wikipedia
+    url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+    soup = BeautifulSoup(url.content, "html.parser")
+    title = soup.find(class_="firstHeading").text
+
+    print(f"{title} \nDo you want to view it? (Y/N)")
+    ans = input("").lower()
+
+    if ans == "y":
+        url = "https://en.wikipedia.org/wiki/%s" % title
+        webbrowser.open(url)
+        break
+    elif ans == "yes":
+        print("Try again!")
+        continue
+    else:
+        print("Wrong choice!")
+        break
